40416570fc568123a713f644df87062cf8df2a1b
Thiago Mendes <thiagomendessan293@gmail.com>
2019-08-19 17:13:52 -0300

    Adicionar Exercicio

--- Exercicio1.py
+++ Exercicio1.py
@@ -1,8 +1,8 @@
 numeros = [1,2,3,4,5,6,7,8,9,10]
 
 
-def verificar():
-    if numeros % 2 == 0:
+for numero in numeros:
+    if numero % 2 == 0:
         print('Numero par')
     else:
         print('Numero impar')
